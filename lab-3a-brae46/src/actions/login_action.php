85d27e90c3559153a92d8da96f210cf78e1627c5
brae46 <70494796+brae46@users.noreply.github.com>
2021-02-10 07:00:52 -0700

    Add files via upload

---
+++ lab-3a-brae46/src/actions/login_action.php
@@ -0,0 +1,83 @@
+<?php
+error_reporting(-1);
+session_start();
+
+
+function error($stringError){ //issue here when echo is first the page doesnt reload and when header is first no error pops up
+	$_SESSION["errorL"] = "$stringError";
+	header("Location:../views/login.php");
+	die();	
+}
+// ./actions/login_action.php
+
+// Read variables and create connection
+$mysql_servername = getenv("MYSQL_SERVERNAME");
+$mysql_user = getenv("MYSQL_USER");
+$mysql_password = getenv("MYSQL_PASSWORD");
+$mysql_database = getenv("MYSQL_DATABASE");
+
+// This section for DEBUGGING ONLY! COMMENT-OUT WHEN FINISHED
+// echo "<p>mysql_servername: $mysql_servername</p>";
+// echo "<p>mysql_user: $mysql_user</p>";
+// echo "<p>mysql_password: $mysql_password</p>";
+// echo "<p>mysql_database: $mysql_database</p>";
+
+$conn = new mysqli($mysql_servername, $mysql_user, $mysql_password, $mysql_database);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+} 
+// else {
+// 	echo "Database Connection Success";
+// }
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// TODO: Log the user in
+//if user exists
+$regStatement = 'SELECT username FROM user WHERE username = ?';
+$stmt = $conn->prepare($regStatement);
+$stmt -> bind_param("s", $username);
+$stmt -> execute();
+$stmt -> bind_result($resultStmt);
+$stmt -> fetch();
+$stmt -> close();
+
+if (!$resultStmt) {
+	error("Username not found");
+}
+
+$hashed_password1 = password_hash($password, PASSWORD_DEFAULT);
+
+$regStatement = 'SELECT password FROM user WHERE username = ?';
+$stmt = $conn->prepare($regStatement);
+$stmt -> bind_param('s', $password); //issue here but idk what. Ive tried hash_password and password
+$stmt -> execute();
+$stmt -> bind_result($resultStmt);
+$hashed_password = $stmt -> fetch();
+$stmt -> close();
+
+//passwords match?
+
+if($hashed_password != $hashed_password1){
+	error("Incorrect Password");
+}
+$_SESSION['username'] = $username;
+$_SESSION['logged_in'] = 1;
+
+$regStatement = 'UPDATE user SET logged_in = ? WHERE username = ?';
+$stmt = $conn->prepare($regStatement);
+$stmt -> bind_param("is", $_SESSION["logged_in"], $_SESSION["username"]); //issue here but idk what. Ive tried hash_password and password
+$stmt -> execute();
+$stmt->close();
+
+// $_SESSION['username'] = $username;
+// $_SESSION['logged_in'] = 1;
+//$_SESSION['id'] = $id;
+	//Set session variables
+header("Location: ../index.php");
+	//send to index.php
+	//set logged_in variables to true	
+
+?>
---
+++ lab-3a-brae46/src/views/login.php
@@ -0,0 +1,60 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>IT210 Lab 3</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"><!-- Links to stylesheets -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>  
+    <link rel="stylesheet" href="src/css/style.css">
+  </head>
+
+  <body>
+    <header>
+      <!--<h1>IT210 Lab 2</h1>-->
+    </header>
+    <nav class="navbar navbar-expand-lg navbarbackground">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a id= navtitle class="nav-link" href="#">IT210 Lab 3 <span class="sr-only">(current)</span></a>
+          </li>
+      </div>
+    </nav>
+    <br>
+    <br>
+    <div class="container">
+      <article>
+        <h2 id=tasktitle >Log Into Account</h2>
+      </article>
+      <br>
+      <ul id="list" class="list-group">
+      </ul>
+      <div>
+        <form action="../actions/login_action.php" method="post">
+          <div class="form-group">
+            <input type="text" class="form-control" name = username id="inputUsername" aria-describedby="emailHelp" placeholder="Username" required/>
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" name = password id="inputPassword" aria-describedby="emailHelp" placeholder="Password" required/>
+          </div>
+          <button id=taskbutton type="submit" class="btn btn-primary">Log In</button>
+        </form>
+          <br>
+        <form action="../views/register.php" method="post"> <!--button doesnt do anything -->
+          <button id=registerButton type="submit" class="btn btn-primary">Register Here</button>
+        </form>
+        <?php
+            echo $_SESSION["errorL"];
+            session_unset();
+          ?>
+      </div>
+    </div> 
+  </body>
+
+</html>
